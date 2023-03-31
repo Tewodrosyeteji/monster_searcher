6feb0b8ad3b336400c380f7c344a8c83e2a51eaf
Tewodrosyeteji <teddytejinesh@gmail.com>
2023-03-31 09:42:08 +0300

    monster searcher functional component done

--- src/App.js
+++ src/App.js
@@ -1,51 +1,88 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import React, { Component } from "react";
+// import React, { Component } from "react";
 import CardList from "./components/card-list/card-list";
 import Search from "./components/search/search";
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchFiled: "",
-    };
-  }
 
-  componentDidMount() {
+const App = () => {
+  const [searchFiled, setSearchFiled] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonster, setFilteredMonster] = useState(monsters);
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return {
-            monsters: users,
-          };
-        })
-      );
-  }
+      .then((users) => setMonsters(users));
+  }, []);
 
-  searchChangeHandler = (event) => {
+  useEffect(() => {
+    const newfilteredMonster = monsters.filter((monster) =>
+      monster.name.toLocaleLowerCase().includes(searchFiled)
+    );
+    setFilteredMonster(newfilteredMonster);
+  }, [monsters, searchFiled]);
+
+  const searchChangeHandler = (event) => {
     const searchString = event.target.value.toLocaleLowerCase();
-    this.setState({
-      searchFiled: searchString,
-    });
+    setSearchFiled(searchString);
   };
-  render() {
-    const filteredMonster = this.state.monsters.filter((monster) =>
-      monster.name.toLocaleLowerCase().includes(this.state.searchFiled)
-    );
 
-    return (
-      <div className="App">
-        <h1 className="app-title"> Monster Search</h1>
-        <Search
-          type="search"
-          placeholder="search monster"
-          changed={this.searchChangeHandler}
-        />
-        <CardList monsters={filteredMonster} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1 className="app-title"> Monster Search</h1>
+      <Search
+        type="search"
+        placeholder="search monster"
+        changed={searchChangeHandler}
+      />
+      <CardList monsters={filteredMonster} />
+    </div>
+  );
+};
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchFiled: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return {
+//             monsters: users,
+//           };
+//         })
+//       );
+//   }
+
+//   searchChangeHandler = (event) => {
+//     const searchString = event.target.value.toLocaleLowerCase();
+//     this.setState({
+//       searchFiled: searchString,
+//     });
+//   };
+//   render() {
+//     const filteredMonster = this.state.monsters.filter((monster) =>
+//       monster.name.toLocaleLowerCase().includes(this.state.searchFiled)
+//     );
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title"> Monster Search</h1>
+//         <Search
+//           type="search"
+//           placeholder="search monster"
+//           changed={this.searchChangeHandler}
+//         />
+//         <CardList monsters={filteredMonster} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 import "./search.css";
 
-class Search extends Component {
-  render() {
-    return (
-      <div className="search">
-        <input
-          type={this.props.type}
-          placeholder={this.props.placeholder}
-          onChange={this.props.changed}
-        />
-      </div>
-    );
-  }
-}
+const Search = ({ type, placeholder, changed }) => {
+  return (
+    <div className="search">
+      <input type={type} placeholder={placeholder} onChange={changed} />
+    </div>
+  );
+};
+// class Search extends Component {
+//   render() {
+//     return (
+//       <div className="search">
+//         <input
+//           type={this.props.type}
+//           placeholder={this.props.placeholder}
+//           onChange={this.props.changed}
+//         />
+//       </div>
+//     );
+//   }
+// }
 
 export default Search;
